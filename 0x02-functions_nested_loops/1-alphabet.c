e72a941ce0f47fef1c2a646046e7dfac5e35475e
Emmanuel Akpaninyang <emmanuelakpaninyang@gmail.com>
2022-07-05 09:50:08 -0700

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*
+* Description print alphbet
+* print_alphabet - prints all alphabets in lowercase.
+* Description: print alphabet in lower
+* Return: On success 1.
+*/
+
+void print_alphabet(void)
+
+{
+
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+
+{
+
+_putchar(i);
+
+}
+_putchar('\n');
+
+}
