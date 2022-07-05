f28feeefc2c8365db6478f4d8a0c433888608c3d
Emmanuel Akpaninyang <emmanuelakpaninyang@gmail.com>
2022-07-05 12:09:12 -0700

    hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
 /*
-*main - program entry point
-
-*Description: 'the program prints putchar'
-
-*return:0 no error, non zero value if error.
-
+* main - prints putchar
+* return: always 0
 */
 
 int main(void)
 {
-printf("_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+return (0)
 }
